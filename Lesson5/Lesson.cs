f21d2f6e20720b78fe330630c3a6002b09e35be8
mimiPillow <erenstonxemin@gmail.com>
2022-09-14 12:12:46 +0300

    lesson5

---
+++ Lesson5/Lesson.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson5
+{
+    class Lesson
+    {
+        static void Main(string[] args)
+        {
+            String data;
+            data = Console.ReadLine();
+
+            Console.WriteLine("data {0}", data);
+
+            //convert
+            int b = 5;
+            int a = Convert.ToInt32(data);
+            Console.WriteLine("sum {0}", a + b);
+            Console.WriteLine();
+
+            //
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("age {0}", age);
+
+        }
+    }
+}
